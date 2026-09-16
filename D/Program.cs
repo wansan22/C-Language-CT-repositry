c9056b2efe8d43d65df9cf05271d8941fb7799fb
fomin <fomin.kirill.2006@mail.ru>
2026-09-16 19:10:20 +0300

    First Work Of OOP

---
+++ D/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Runtime;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public class BankAccount
+    {
+        private int accountNumber;
+        private string owner = string.Empty;
+        private decimal balance;
+        private static int counter = 0;
+        public BankAccount(string ownernew)
+        {
+            Console.WriteLine($" кол-во экземпляров {BankAccount.counter}");
+            owner = ownernew;
+            counter += 1;
+            accountNumber = GenerateAccountNumber();
+            balance = 0;
+
+        }
+        public void Deposit(int amount)
+        {
+            if (isValidAmount(amount) == true)
+            {
+                balance += amount;
+                Console.WriteLine($"Баланс успешно пополнен текущая сумма {balance}");
+            }
+           else
+            {
+                Console.WriteLine("данную сумму нельзя пополнить на баланс");
+            }
+        }
+        public void Withdraw(int amount)
+        {
+           if (isValidAmount(amount) == true)
+            {
+                
+                if ( balance < amount)
+                {
+                    Console.WriteLine("Недостаточно средств для снятия");
+                }
+                else
+                {
+                    balance -= amount;
+                    Console.WriteLine($"сумма {amount} была успешно снята остаток на балансе {balance}");
+                } 
+            }
+           else
+            {
+                Console.WriteLine("данную сумму нельзя пополнить на баланс");
+            }
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine(balance);
+        }
+        private bool isValidAmount(int amount)
+        {
+            if ( amount > 0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int GetTotalCounts()
+        {
+          
+                return BankAccount.counter;
+        }
+        public static int GenerateAccountNumber()
+        {
+            Random generate = new Random();
+            var generatenumber = generate.Next(1,100);
+            return generatenumber;
+            
+            
+        }
+        public void GetAccountInfo()
+        {
+            Console.WriteLine($"{owner} , {accountNumber}");
+        }
+    }
+    private static void Main()
+    {
+        BankAccount bankacc = new BankAccount("Олег") ;
+        BankAccount bankacc1 = new BankAccount("Олег") ;
+        bankacc.Deposit(100);
+        bankacc.Withdraw(50);
+        bankacc.GetBalance();
+        bankacc.GetAccountInfo();
+        BankAccount.GetTotalCounts();
+       
+
+    }
+}
--- obj/Debug/net10.0/TreeCT.AssemblyInfo.cs
+++ obj/Debug/net10.0/TreeCT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeCT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e6f74db88059fb7afbfb3aab6d7f1b9f2a1ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253474ec070b374fe5410a5bff04f3a76ad87275")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeCT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeCT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
